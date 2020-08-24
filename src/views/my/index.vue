4c286d44f6ee1d0781df038ac25fb27915c43ddf
gavyn-yan <1434437175@qq.com>
2020-08-24 22:36:57 +0800

    完善了登录页面

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,61 +1,126 @@
 <template>
   <div class="my-container">
-    <div class="header not-login">
-      <div class="login-btn" @click="$router.push('/login')">
-        <img class="mobile-img" src="~@/assets/mobile.png" alt="">
-        <span class="text">登录/注册</span>
-      </div>
-    </div>
-    <div class="header user-info">
+      <!-- 已登录 -->
+    <div v-if="user" class="header user-info">
       <div class="base-info">
         <div class="left">
             <van-image
               class="avatar"
-              src="https://img.yzcdn.cn/vant/cat.jpeg"
+              src="userInfo.photo"
               round
               fit="cover"
             />
-            <span class="name">黑马头条号</span>
+            <span class="name">{{ userInfo.name }}</span>
         </div>
         <div class="right">
             <van-button size="mini" round>编辑资料</van-button>
         </div>
       </div>
       <div class="data-stats">
-          <div class="data-item">
-              <div class="count">10</div>
-              <div class="text">头条</div>
-          </div>
-          <div class="data-item">
-              <div class="count">66</div>
-              <div class="text">关注</div>
-          </div>
-          <div class="data-item">
-              <div class="count">88</div>
-              <div class="text">粉丝</div>
-          </div>
-          <div class="data-item">
-              <div class="count">88</div>
-              <div class="text">获赞</div>
-          </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.art_count }}</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.follow_count }}</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.fans_count }}</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">{{ userInfo.like_count }}</span>
+          <span class="text">获赞</span>
+        </div>
       </div>
     </div>
+    <!-- 已登录 -->
+
+    <!-- 未登录 -->
+    <div v-else class="header not-login">
+      <div class="login-btn" @click="$router.push('/login')">
+        <img class="mobile-img" src="~@/assets/mobile.png" alt="">
+        <span class="text">登录/注册</span>
+      </div>
+    </div>
+    <!-- 未登录 -->
+
+    <!-- 导航 -->
+    <van-grid class="grid-nav" :column-num="2" clickable>
+      <van-grid-item class="grid-item" >
+          <i slot="icon"
+          class="toutiao toutiao-shoucang"></i>
+          <span slot="text" class="text">收藏</span>
+      </van-grid-item>
+      <van-grid-item class="grid-item" >
+          <i slot="icon"
+          class="toutiao toutiao-lishi"></i>
+          <span slot="text" class="text">历史</span>
+      </van-grid-item>
+    </van-grid>
+
+    <!-- 导航 -->
+    <van-cell title="消息通知" is-link />
+    <van-cell class="mb-9" title="小智同学" is-link />
+    <van-cell
+      v-if="user"
+      class="logout-cell"
+      clickable
+      title="退出登录"
+      @click="onLogout"
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { getUserInfo } from '@/api/user'
+
 export default {
   name: 'MyIndex',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      userInfo: {}
+    }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
-  created () {},
+  created () {
+    // 如果用户登录了，则请求加载用户信息数据
+    if (this.user) {
+      this.loadUserInfo()
+    }
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    onLogout () {
+      // 退出提示
+      // 在组件中需要使用 this.$dialog 来调用弹  框组件
+      this.$dialog.confirm({
+        title: '确认退出吗？'
+      }).then(() => {
+        // on confirm
+        // 确认退出：清除登录状态（容器中的 user   + 本地存储中的 user）
+        this.$store.commit('setUser', null)
+      }).catch(() => {
+        // on cancel
+        console.log('取消执行这里')
+      })
+    },
+    async loadUserInfo () {
+      try {
+        const { data } = await getUserInfo()
+        this.userInfo = data.data
+      } catch (err) {
+        this.$toast('获取数据失败，请稍后重试')
+      }
+    }
+  }
 }
 </script>
 
@@ -128,6 +193,27 @@ export default {
                 }
             }
         }
+    }
+    .grid-nav {
+        .grid-item {
+            height: 141px;
+            i.toutiao {
+                font-size: 45px;
+            }
+            .toutiao-shoucang {
+                color: #eb5253;
+            }
+            .toutiao-lishi {
+                color: #ff9d1d;
+            }
+            span.text {
+                font-size: 28px;
+            }
+        }
+    }
+    .logout-cell {
+      text-align: center;
+      color: #d86262;
     }
 }
 </style>
